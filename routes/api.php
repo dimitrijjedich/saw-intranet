b940e90d2d713a9d4ae6581027ab92cfeb544eec
dimitrijjedich <dimitrij@startup-werk.de>
2024-07-17 17:49:52 +0200

    fix route

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/api/users', function () {
+Route::get('users', function () {
     return User::all();
 });
