cad9592db02fac1ce3c61eb2bbbb00292f58391a
TrueRihot <info@theilweise.de>
2024-07-23 22:42:24 +0200

    Bisschen aufräumen und css

---
+++ frontend/src/components/main.tsx
@@ -0,0 +1,11 @@
+export default function Main() {
+    return (
+        <>
+            <h1>DIMI IST EIN KRASSER TYP JA</h1>
+            <h2>Ja ja</h2>
+            <h3>Man man</h3>
+            <h4>Wird er eher krasser oder noch krasser?</h4>
+            <p>hmm</p>
+        </>
+    );
+}
