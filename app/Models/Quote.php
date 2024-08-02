05a79ce62950e5bde13ca915acb0d5f51bd13f33
dimitrijjedich <dimitrij@startup-werk.de>
2024-08-02 16:30:05 +0200

    add dispatching of QuoteEvent on created of Quote model

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Events\QuoteEvent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Quote extends Model
 {
     use HasFactory;
+
+    protected $dispatchesEvents = [
+        'created' => QuoteEvent::class,
+    ];
 }
