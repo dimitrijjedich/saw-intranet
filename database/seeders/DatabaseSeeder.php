16b50c07d054c0edf94229ea0ec07ab0af9ccdd2
dimitrijjedich <dimitrij@startup-werk.de>
2024-08-03 10:19:15 +0200

    remove unused import

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Quote;
 use App\Models\User;
-use Database\Factories\QuoteFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
