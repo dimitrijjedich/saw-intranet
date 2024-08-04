d046c5bd93981fb084591886da47064de2133686
moritztheil <moritz@startup-werk.de>
2024-08-04 15:08:57 +0200

    TODO

--- frontend/src/components/ui/BaseModuleWrapper/BaseModuleWrapper.tsx
+++ frontend/src/components/ui/BaseModuleWrapper/BaseModuleWrapper.tsx
@@ -5,6 +5,7 @@ interface BaseModuleWrapperProps {
 }
 
 export default function BaseModuleWrapper(props: BaseModuleWrapperProps) {
+    //TODO: Implement this https://codesandbox.io/s/cisbc
     return (
         <div className={'module-wrapper'}>
             {props.children}
