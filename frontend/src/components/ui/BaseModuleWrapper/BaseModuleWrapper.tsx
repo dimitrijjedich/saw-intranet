fce5d554975135416d4afa227689ec8b49fe42fb
moritztheil <moritz@startup-werk.de>
2024-08-13 10:11:43 +0200

    spring basic stuffs

--- frontend/src/components/ui/BaseModuleWrapper/BaseModuleWrapper.tsx
+++ frontend/src/components/ui/BaseModuleWrapper/BaseModuleWrapper.tsx
@@ -5,7 +5,6 @@ interface BaseModuleWrapperProps {
 }
 
 export default function BaseModuleWrapper(props: BaseModuleWrapperProps) {
-    //TODO: Implement this https://codesandbox.io/s/cisbc
     return (
         <div className={'module-wrapper'}>
             {props.children}
